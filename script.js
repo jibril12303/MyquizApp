869bb4837a55f10202f5bf938769799f2d055c26
jibril yusuf <jibril_2012@live.com>
2022-01-15 21:27:47 +0000

    javascript working on

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const start_btn = document.querySelector(".start_btn button");
 const rule_box = document.querySelector(".rule_box");
+const exit_btn = rule_box.querySelector(".buttons .quit");
+const continue_btn = rule_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");

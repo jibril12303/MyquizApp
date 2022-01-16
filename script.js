0f1657074997d50cd25b49adc0bf696d060749c4
jibril yusuf <jibril_2012@live.com>
2022-01-16 02:13:22 +0000

    quiz box css completed

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
+
+start_btn.onclick = ()=>{
+  rule_box.classList.add("activeInfo"); 
+}
+
+exit_btn.onclick = ()=>{
+  rule_box.classList.remove("activeInfo"); 
+}
+
+continue_btn.onclick = ()=>{
+  rule_box.classList.remove("activeInfo"); 
+  quiz_box.classList.add("activeQuiz"); 
+  showQuetions(0);
+  queCounter(1);
+  startTimer(5);
+  startTimerLine(0);
+}
+
+let timeValue =  5;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;

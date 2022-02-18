438e795773dad3f208b7dfd769d73d46e32d6ac8
jibril yusuf <jibril_2012@live.com>
2022-02-18 19:10:41 +0000

    code wasnt working fixed the problem

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many countries in Africa?",
+    answer: "54 countries",
+    options: [
+      "54 countries",
+      "40 countries",
+      "55 countries",
+      "57 countries"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the capital of United Kingdom?",
+    answer: "London",
+    options: [
+      "Skopje",
+      "Vienna",
+      "London",
+      "Sarajevo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the population in America?",
+    answer: "329.5 million (2020)",
+    options: [
+      "379.5 million (2020)",
+      "229.5 million (2020)",
+      "329.5 million (2020)",
+      "390 million (2020)"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the capital of Senegal?",
+    answer: "Dakar",
+    options: [
+      "Dakar",
+      "Dhaka",
+      "Lahore",
+      "Beirut"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the capital of Republic of San Marino?",
+    answer: "San Marino",
+    options: [
+      "Vilnius",
+      "Berne",
+      "Dublin",
+      "San Marino"
+    ]
+  },
+  
+];
